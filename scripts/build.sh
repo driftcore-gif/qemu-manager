adf64dd501fee8cacf6c813d16521a80e24f632c
driftcore-gif <jishanshaikh20113@gmail.com>
2026-06-30 12:30:14 +0530

    Delete scripts directory

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "[QEMU Manager] Setting up build..."
-meson setup build --wipe 2>/dev/null || meson setup build
-echo "[QEMU Manager] Building..."
-cd build && ninja
-echo ""
-echo "Build complete! Run with: ./build/qemu-manager"
